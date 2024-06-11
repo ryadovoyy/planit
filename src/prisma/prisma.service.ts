dbdb86bc8be7f4cc41fc2695d0e34337dcbfe73c
ryadovoyy <holstinindvs@gmail.com>
2024-06-12 02:20:44 +0700

    feat(prisma): add global module and configure logging

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, 'query'>
+  implements OnModuleInit
+{
+  constructor() {
+    let log: (Prisma.LogLevel | Prisma.LogDefinition)[];
+
+    if (process.env.NODE_ENV === 'development') {
+      log = [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+        {
+          emit: 'stdout',
+          level: 'error',
+        },
+      ];
+    } else {
+      log = ['error'];
+    }
+
+    super({ log });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+
+    this.$on('query', (e) => {
+      console.log(`Query: ${e.query}`);
+      console.log(`Params: ${e.params}`);
+      console.log(`Duration: ${e.duration}ms`);
+    });
+  }
+}
